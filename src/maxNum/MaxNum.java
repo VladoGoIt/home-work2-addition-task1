65bc5a4c7e1f0e7c54ee7f2df56f84d6205c4175
VladoGoIt <dzhuhalyk@gmail.com>
2017-07-01 23:07:32 +0300

    створив новий проект і написав початок циклу

---
+++ src/maxNum/MaxNum.java
@@ -0,0 +1,20 @@
+package maxNum;
+
+import java.util.Scanner;
+
+/**
+ * Created by group on 01.07.2017.
+ */
+public class MaxNum {
+    public static void main(String[] args){
+        Scanner in = new Scanner (System.in);
+        int[] m = new int[4];
+
+        for (int i = 0; i < m.length; i++ )
+            m[i] = in.nextInt();
+
+        
+
+    }
+
+}
